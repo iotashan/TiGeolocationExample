81f170cac9adef57fe1bcead2287c20f36c38fd8
Shannon Hicks <shicks@pintley.com>
2012-04-05 15:27:42 -0500

    first commit

---
+++ Resources/GeoHelper.js
@@ -0,0 +1,100 @@
+var osname = Ti.Platform.osname;
+var osversion = parseFloat(Titanium.Platform.version);
+
+var GeoHelper = function(_args) {
+	var geoHelper = {};
+	
+	// args proxy
+	geoHelper.onSuccess = _args.onSuccess;
+	geoHelper.onError = _args.onError;
+	geoHelper.purpose = _args.purpose ? _args.purpose : 'This message describes what you use geolocation services for.';
+	geoHelper.minAccuracy = _args.minAccuracy ? _args.minAccuracy : 100; // once we get within 100 meters, that's good enough 
+	geoHelper.timeout = _args.timeout ? _args.timeout : 30; // if you don't have what you need in 30 seconds, the user probably doesn't want to wait any more
+	geoHelper.maxAge = _args.maxAge ? _args.maxAge : 0; // if the last good position was withing [maxAge] seconds, don't bother turning on the radios
+	geoHelper.onUpdate = _args.onUpdate; // this callback is for debug purposes
+	
+	// data containers
+	var lastPosition;
+	var lastGoodPosition;
+	var timeoutCall;
+	
+	// flags
+	geoHelper.geolocating = false;
+	
+	if (osname == 'iphone') {
+		if (osversion >= 3.2) {
+			Ti.Geolocation.purpose = geoHelper.purpose;
+		}
+	}
+
+	if (osname == 'android') {
+		Ti.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_HIGH;
+	} else {
+		Ti.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+	}
+	
+	geoHelper.findPosition = function(){
+		// check to see if we can just recycle old data
+		if (lastGoodPosition && lastGoodPosition.coords.timestamp + maxAge*1000 >= new Date().getTime()) {
+			Ti.API.debug('using cached geolocation');
+			onSuccess(lastGoodPosition);
+		} else {
+			// ok, we have to turn on some battery-draining stuff
+			Ti.API.debug('starting to geolocate');
+			Ti.Geolocation.addEventListener('location',geoFound);
+			timeoutCall = setTimeout(function(){
+				Ti.API.debug('geolocation timed out, canceling');
+				geoHelper.cancel();
+				if (geoHelper.onError) {
+					geoHelper.onError(lastPosition);
+				}
+			},geoHelper.timeout*1000);
+		}
+	}
+	
+	geoHelper.cancel = function(){
+		Ti.API.debug('bye bye geolocate');
+		Ti.Geolocation.removeEventListener('location',geoFound);
+		clearTimeout(timeoutCall);
+	};
+	
+	var geoFound = function(_geo){
+		Ti.API.debug('geoEvent: ' + JSON.stringify(_geo));
+		if (geoHelper.onUpdate) {
+			geoHelper.onUpdate(_geo);
+		}
+		if (_geo.success) {
+			lastPosition = _geo;
+			if (lastPosition.coords.accuracy <= geoHelper.minAccuracy) {
+				clearTimeout(timeoutCall);
+				Ti.Geolocation.removeEventListener('location',geoFound);
+				Ti.API.debug('using fresh geolocation');
+				lastGoodPosition = lastPosition;
+				geoHelper.onSuccess(lastGoodPosition);
+			}
+		}
+	};
+	
+	// handle android pause/stop/resume events
+	if (osname == 'android') {
+		Ti.Android.currentActivity.addEventListener('pause', function() {
+			if (geoHelper.geolocating) {
+				geoHelper.stopGeo();
+			}
+		});
+		Ti.Android.currentActivity.addEventListener('stop', function() {
+			if (geoHelper.geolocating) {
+				geoHelper.stopGeo();
+			}
+		});
+		Ti.Android.currentActivity.addEventListener('resume', function() {
+			if (geoHelper.geolocating) {
+				geoHelper.startGeo();
+			}
+		});
+	};
+	
+	return geoHelper;
+};
+
+module.exports = GeoHelper;
---
+++ Resources/app.js
@@ -0,0 +1,68 @@
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Ti.UI.setBackgroundColor('#000');
+
+var GeoHelper = require('GeoHelper');
+
+var win = Ti.UI.createWindow({  
+    title:'Geolocation Example',
+    backgroundColor:'#fff',
+});
+
+var geoButton = Ti.UI.createButton({
+	title:'Get my Location',
+	top:10,
+	height:60,
+	width:250,
+});
+win.add(geoButton);
+
+var debugBox = Ti.UI.createTextArea({
+	bottom:10,
+	top:80,
+	left:10,
+	right:10,
+	borderColor:'#000',
+	font:{size:10},
+	editable:false,
+	value:'Log:\n',
+});
+win.add(debugBox);
+
+var myGeoHelper = new GeoHelper({
+	onSuccess:function(e) {
+		debugBox.value += 'Success: ' + JSON.stringify(e) + '\n';
+		waitDialog.hide();
+		// display an alert on a delay so waitDialog has time to close
+		setTimeout(function(){
+			alert('You are at '+e.coords.latitude+','+e.coords.longitude);
+		},500);
+	},
+	onError:function(e) {
+		debugBox.value += 'Error: ' + JSON.stringify(e) + '\n';
+		waitDialog.hide();
+	},
+	onUpdate:function(e) {
+		debugBox.value += 'Update: ' + JSON.stringify(e) + '\n';
+	},
+	purpose:'Geolocation Demo',
+	minAccuracy:50,
+	maxAge:45,
+});
+
+var waitDialog = Titanium.UI.createAlertDialog({
+    title: '',
+    message: 'Getting your phone\'s location',
+    buttonNames: ['Cancel'],
+    cancel:0
+});
+
+waitDialog.addEventListener('click',function(){
+	myGeoHelper.cancel();
+});
+
+geoButton.addEventListener('click',function(){
+	waitDialog.show();
+	myGeoHelper.findPosition();
+});
+
+win.open();
